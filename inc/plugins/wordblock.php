7b4a98fdaeb86cdbe5dfa0edca0ba4c4255455d0
dragonexpert <arceus@teamdimensional.net>
2016-10-24 10:35:34 -0500

    Update wordblock.php

--- inc/plugins/wordblock.php
+++ inc/plugins/wordblock.php
@@ -52,6 +52,7 @@ function wordblock_uninstall()
 
 // Hooks
 $plugins->add_hook("datahandler_post_validate_post", "wordblock_datahandler_post_validate_post");
+$plugins->add_hook("datahandler_post_validate_thread", "wordblock_datahandler_post_validate_thread");
 $plugins->add_hook("admin_config_menu", "wordblock_admin_config_menu");
 $plugins->add_hook("admin_config_action_handler", "wordblock_admin_config_action_handler");
 
@@ -62,6 +63,26 @@ function wordblock_datahandler_post_validate_post($this)
     while($word = $db->fetch_array($query))
     {
         if(strpos($post['message'], $word['word']) !== false)
+        {
+            // Update the last use column
+            $update_array = array(
+                "lastattempt" => time()
+            );
+            $wid = $word['wid'];
+            $db->update_query("wordblock", $update_array, "wid=$wid");
+            error("Your post contains one or more words that are not allowed.");
+        }
+    }
+    return;
+}
+
+function wordblock_datahandler_post_validate_thread($this)
+{
+    global $thread, $db;
+    $query = $db->simple_select("wordblock", "*");
+    while($word = $db->fetch_array($query))
+    {
+        if(strpos($thread['message'], $word['word']) !== false)
         {
             // Update the last use column
             $update_array = array(
